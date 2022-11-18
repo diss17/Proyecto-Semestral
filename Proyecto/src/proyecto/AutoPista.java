276685c7310061a77a2461b0a8782a9ccbcc43d1
Gaspar Jimenez <oficialgaspii@gmail.com>
2022-11-18 16:55:18 -0300

    implementacion primera autopista(sujeta a cambios)+preparativos primera revision

---
+++ Proyecto/src/proyecto/AutoPista.java
@@ -0,0 +1,22 @@
+package proyecto;
+
+import java.awt.*;
+
+public class AutoPista {
+
+    private int x;
+    private int y;//Coordenada x e y de la autopista
+
+    public AutoPista(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+    public void paint(Graphics g){
+        g.setColor(new Color(159, 159, 159));
+        g.fillOval(x, y, 700, 700);
+        g.setColor(new Color(24, 129, 55));
+        g.fillOval(x + 150, y + 150, 400, 400);
+        g.setColor(Color.WHITE); 
+    }
+
+}
--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -3,7 +3,7 @@ package proyecto;
 public class Proyecto {
 
     public static void main(String[] args) {
-        
+        new Ventana();
     }
 
 }
