90714364dde31bb977af13af114e9d6303d039c1
Gaspar Jimenez <oficialgaspii@gmail.com>
2022-11-18 17:07:37 -0300

    Clase PanelPrincipal creada proxima a ser rellenada

---
+++ Proyecto/src/proyecto/PanelPrincipal.java
@@ -0,0 +1,47 @@
+package proyecto;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class PanelPrincipal extends JPanel implements ActionListener,KeyListener{
+    public PanelPrincipal(){
+
+    }
+    public void frenos(){
+
+    }
+    public void derecha(){
+
+    }
+    public void izquieda(){
+
+    }
+    public void pasa_cambios(){
+
+    }
+    public void paint(){
+
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+}
