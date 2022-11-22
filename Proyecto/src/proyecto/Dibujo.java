c1b92c448ab16c70bc0c2c2ce91d9be887b00fcd
Franchesca Mora <francmora2021@inf.udec.cl>
2022-11-22 16:56:11 -0300

    Se ordena clase Dibujo y se agregan los movimientos del vehiculo

--- Proyecto/src/proyecto/Dibujo.java
+++ Proyecto/src/proyecto/Dibujo.java
@@ -4,34 +4,27 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class Dibujo extends JPanel {
+    private int x=117,y=120;
+    public int aux_x;
+    public int aux_y;
     public Dibujo(){
         this.setBackground(Color.cyan);    
+    }
+    
+    public int gettX(){
+        this.aux_x=x;
+        return x;
+        
+    }
+    public int gettY(){
+        this.aux_y=y;
+        return y;
     }
     
     @Override
     public void paint(Graphics g){ //se agrega para pintar rectángulo rojo
        super.paint(g);
-       //Auto
-       g.setColor(Color.red);      //se unta el pincel g en color rojo
-       g.fillRect(117,120,70,30);    //se le dice al pincel pinte rectángulo
-       
-       
-       //Ruedas
-       g.setColor(Color.DARK_GRAY); 
-       g.fillOval(120,115,26,10);
-       g.setColor(Color.DARK_GRAY);
-       g.fillOval(120,145,26,10);
-       g.setColor(Color.DARK_GRAY);
-       g.fillOval(157,115,26,10);
-       g.setColor(Color.DARK_GRAY);
-       g.fillOval(157,145,26,10);
-       
-       //Focos autito.
-       g.setColor(Color.yellow);
-       g.fillOval(180,137,10,10);
-       g.setColor(Color.yellow);
-       g.fillOval(180,123,10,10);
-       
+       //PAINT CARRETERA.
        //Carretera idk-RECTOS
        g.setColor(Color.LIGHT_GRAY);
        g.fillRect(200,100,330,80);
@@ -64,7 +57,53 @@ public class Dibujo extends JPanel {
        
        g.setColor(Color.LIGHT_GRAY);
        g.fillArc(410, 400, 180, 230, -90,180);
+       
+       //PAINT AUTO.
+       g.setColor(Color.red);      //se unta el pincel g en color rojo
+       g.fillRect(x,y,70,30);    //se le dice al pincel pinte rectángulo
+       
+       
+       //Ruedas
+       g.setColor(Color.DARK_GRAY); 
+       g.fillOval(x+3,y-5,26,10);
+       g.setColor(Color.DARK_GRAY);
+       g.fillOval(x+3,y+25,26,10);
+       g.setColor(Color.DARK_GRAY);
+       g.fillOval(x+40,y-5,26,10);
+       g.setColor(Color.DARK_GRAY);
+       g.fillOval(x+40,y+25,26,10);
+       
+       //Focos autito.
+       g.setColor(Color.yellow);
+       g.fillOval(x+63,y+17,10,10);
+       g.setColor(Color.yellow);
+       g.fillOval(x+63,y+3,10,10);
      
        
     }  
+    
+    //DIRECCIONES DEL VEHICULO CON TECLAS.
+    public void MoverArriba(){
+        if(y>=0){
+            y = y-10;
+        }
+    }
+    
+    public void MoverDerecha(){
+        if(x<=730){
+            x=x+10;
+        }
+    }
+    
+    public void MoverIzquierda(){
+        if(x>=30){
+            x=x-10;
+        }
+    }
+    
+    public void MoverAbajo(){
+        if(y<=730){
+            y=y+10;
+        }
+    }
 }
