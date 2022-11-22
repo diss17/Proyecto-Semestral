860990724fa302ce746716c8bdefd7bd39809ba3
Franchesca Mora <francmora2021@inf.udec.cl>
2022-11-22 17:01:08 -0300

    Se actualiza clase Ventana

--- Proyecto/src/proyecto/Ventana.java
+++ Proyecto/src/proyecto/Ventana.java
@@ -1,16 +1,76 @@
 package proyecto;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.*;
 
-public class Ventana extends JFrame {
+public class Ventana extends JFrame implements ActionListener,KeyListener {
+    Dibujo dibujo = new Dibujo();
     public Ventana(){
         setSize(800,800);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.getContentPane().setBackground(Color.GREEN);
-        this.add(new Dibujo( ),BorderLayout.CENTER);
+        this.add(dibujo,BorderLayout.CENTER);
         this.setVisible(true);
+        this.addKeyListener(this);
+        this.setFocusable(true);
    
+    }
+    
+     public void frenos(){
+
+    }
+    public void derecha(){
+
+    }
+    public void izquieda(){
+
+    }
+    public void pasa_cambios(){
+
+    }
+    public void paint(){
+
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        switch(e.getKeyCode()){
+            case 40 -> { 
+                dibujo.MoverAbajo();
+                repaint();
+            }
+            case 38 -> {
+                dibujo.MoverArriba();
+                repaint();
+            }
+            case 39 -> {
+                dibujo.MoverDerecha();
+                repaint();
+            }
+            case 37 -> {
+                dibujo.MoverIzquierda();
+                repaint();
+            }
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
     }
     
 }
