c21715b3fdf68594c4a6c1d810425f6943ef413d
Franchesca Mora <francmora2021@inf.udec.cl>
2022-11-26 23:47:39 -0300

    Arreglo en pixeles de la pista

--- Proyecto/src/Objetos/AutoPista.java
+++ Proyecto/src/Objetos/AutoPista.java
@@ -11,42 +11,54 @@ public class AutoPista {
     public void paint(Graphics g) {
         //PAINT CARRETERA.
         //Carretera RECTAS
+        //Pista recta 1
         g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(200, 50, 800, 100);
+        g.fillRect(200, 60, 1020, 100);
 
+        //Pista recta 2
         g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(300, 200, 700, 100);
+        g.fillRect(320, 246, 900, 100);
 
+        //Pista recta 3
         g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(300, 350, 700, 100);
+        g.fillRect(320, 433, 910, 100);
 
+        //Pista recta 4
         g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(200, 500, 800, 100);
+        g.fillRect(200, 620, 1030, 100);
 
+        //Recta de la izquierda
         g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(100, 150, 100, 350);
+        g.fillRect(100, 160, 100, 460);
 
+        
         //Linea Salida
         g.setColor(Color.black);
-        g.fillRect(590, 50, 30, 100);
+        g.fillRect(590, 60, 30, 100);
         //Linea de Meta
         g.setColor(Color.white);
-        g.fillRect(560, 50, 30, 100);
+        g.fillRect(560, 60, 30, 100);
 
         //Carretera CURVAS
         //(x,y,tamaño ancho,largo,arco de inicio en grd, arco final en grd, tipo de arco)
+        
         g.setColor(Color.LIGHT_GRAY);
-        g.fillArc(100, 400, 210, 200, -180, 90);
+        //Curva baja pista izquierda
+        g.fillArc(100, 520, 210, 200, -180, 90);
+        
+        //Curva arriba pista izquierda
         g.setColor(Color.LIGHT_GRAY);
-        g.fillArc(100, 50, 210, 200, 180, -90);
+        g.fillArc(100, 60, 210, 200, 180, -90);
+
+        //Tres curvas restantes
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillArc(1120, 60, 200, 286, 90, -180);
 
         g.setColor(Color.LIGHT_GRAY);
-        g.fillArc(900, 50, 190, 250, 90, -180);
-
+        g.fillArc(230, 246, 200, 287, 90, 180);
+        
         g.setColor(Color.LIGHT_GRAY);
-        g.fillArc(210, 200, 190, 250, 90, 180);
-        g.setColor(Color.LIGHT_GRAY);
-        g.fillArc(900, 350, 190, 250, 90, -180);
+        g.fillArc(1120, 433, 210, 286, 90, -180);
 
     }
 
